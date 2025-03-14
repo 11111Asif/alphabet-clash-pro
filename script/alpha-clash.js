196dc4297c425d1ec9ce381c23ccdf90ba26de54
11111Asif <asif040502@gmail.com>
2025-03-13 21:06:17 -0700

    added to color

--- script/alpha-clash.js
+++ script/alpha-clash.js
@@ -10,6 +10,11 @@
 function continueGame(){
     const alphabet = getARandomAlphabet();
     console.log('your random alphabet', alphabet)
+
+    const currentAlphabetElement = document.getElementById('current-alphabet')
+    currentAlphabetElement.innerText = alphabet;
+
+   setBackgroundColorById(alphabet);
 }
 
 function play(){
